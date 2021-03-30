4af31000d08a5572223ad85bad6ced924474fb22
thuve92 <72792411+thuve92@users.noreply.github.com>
2021-03-30 12:23:39 +0200

    Initial commit

---
+++ Rook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace oblig2
+{
+    class Rook : Piece
+    {
+        public Rook() :base("TRN")
+        {
+            
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+
+        }
+    }
+    
+    
+
+}
+
